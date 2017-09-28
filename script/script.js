ffe2ad41a76e97e593b9089eeeb09318afe21ad3
zadiacuza <andy@banachwebdesign.nl>
2017-09-28 13:07:41 +0200

    dsadsad

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
+    $("nav a").click(function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
 });
