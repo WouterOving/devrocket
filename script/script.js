b4308b340832b9c750b06af92052f0c86acf413d
KevinSpanish <31925187+KevinSpanish@users.noreply.github.com>
2017-10-04 09:34:04 +0200

    added slider
    jas

--- script/script.js
+++ script/script.js
@@ -14,5 +14,70 @@ $(document).ready(function () {
     
     $("nav a").click(function() {
         $('nav ul').removeClass("open_menu");
-    })
+    });
+	
+	$(".ett").click(function(){
+		$(".een").find(".lid_text").toggle('fade');
+		$(".een").next().removeClass("persoon_toggle");
+		$(".een").toggleClass("persoon_toggle");
+		$(".een").find(".lid h2").toggle('blind');
+		$(".een").find(".lid img").toggleClass("persoon_img_toggle");
+		$(".portfolio_een").hide('blind');
+		$(".portfolio").removeClass("portfolio_toggle");
+	});
+	
+	$(".tva").click(function(){
+		$(".twee").find(".lid_text").toggle('fade');
+		$(".twee").next().removeClass("persoon_toggle");
+		$(".twee").toggleClass("persoon_toggle");
+		$(".twee").find(".lid h2").toggle('blind');
+		$(".twee").find(".lid img").toggleClass("persoon_img_toggle");
+		$(".portfolio_twee").hide('blind');
+		$(".portfolio").removeClass("portfolio_toggle");
+		
+		$(".een").toggleClass("m33");
+	});
+	
+	$(".tre").click(function(){
+		$(".drie").find(".lid_text").toggle('fade');
+		$(".drie").next().removeClass("persoon_toggle");
+		$(".drie").toggleClass("persoon_toggle");
+		$(".drie").find(".lid h2").toggle('blind');
+		$(".drie").find(".lid img").toggleClass("persoon_img_toggle");
+		$(".portfolio_drie").hide('blind');
+		$(".portfolio").removeClass("portfolio_toggle");
+		
+		$(".een").toggleClass("m66");
+		$(".twee").toggleClass("m33");
+	});
+	
+	$("#btn_alpay").click(function(){
+		$(".portfolio").toggleClass("portfolio_toggle");
+		$("#portfolio_alpay").toggle('blind');
+	});
+	
+	$("#btn_sam").click(function(){
+		$(".portfolio").toggleClass("portfolio_toggle");
+		$("#portfolio_sam").toggle('blind');
+	});
+	
+	$("#btn_wouter").click(function(){
+		$(".portfolio").toggleClass("portfolio_toggle");
+		$("#portfolio_wouter").toggle('blind');
+	});
+	
+	$("#btn_andy").click(function(){
+		$(".portfolio").toggleClass("portfolio_toggle");
+		$("#portfolio_andy").toggle('blind');
+	});
+	
+	$("#btn_kevin").click(function(){
+		$(".portfolio").toggleClass("portfolio_toggle");
+		$("#portfolio_kevin").toggle('blind');
+	});
+	
+	$("#btn_thom").click(function(){
+		$(".portfolio").toggleClass("portfolio_toggle");
+		$("#portfolio_thom").toggle('blind');
+	});
 });
