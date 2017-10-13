2b8c44d40951d65a3cd6bd180e673897a0b4a5a3
zadiacuza <andy@banachwebdesign.nl>
2017-10-13 11:17:04 +0200

    sdfdsaf
    sdfsaf

--- script/script.js
+++ script/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
+    
+    $(window).scroll(function(){
+        var scrollTop = $(this).scrollTop();
+        $('#background').css('top', -(scrollTop / 4) + 'px');
+    });
+    
     $("nav a").click(function (event) {
         event.preventDefault();
 
         $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
+            scrollTop: $($.attr(this, 'href')).offset().top + (-60)
         }, 500);
     });
     
@@ -14,6 +20,10 @@ $(document).ready(function () {
     $("nav a").click(function() {
         $('nav ul').removeClass("open_menu");
     });
+    
+    $(".sider button:nth-child(1)").click {
+        
+    }
     
     
 });
